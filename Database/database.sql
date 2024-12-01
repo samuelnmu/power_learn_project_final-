9d3693df61ebfa3d761bd7331ae1616447c7b16d
samelnmu <samelnmu@gmail.com>
2024-12-01 15:03:36 +0300

    dbjsCommit

--- Database/database.sql
+++ Database/database.sql
@@ -1,3 +1,12 @@
+CREATE TABLE User (
+    user_id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(50) NOT NULL,
+    middle_name VARCHAR(50),
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
 CREATE TABLE Investor (
     investor_id INT PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(50) UNIQUE NOT NULL,
